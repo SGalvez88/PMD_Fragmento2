97a1ec535d417048d3d1c0e90ccdd5687e771104
sgalvez88 <sergio.galvez.reguera@iestubalcain.net>
2023-01-18 14:16:42 +0100

    initial commit

--- app/src/main/java/com/example/pmd_fragmento2/Fragmento2.java
+++ app/src/main/java/com/example/pmd_fragmento2/Fragmento2.java
@@ -1,6 +1,18 @@
 package com.example.pmd_fragmento2;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class Fragmento2 extends Fragment {
+
+    @Override
+    public View onCreateView( LayoutInflater inflater,  ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragmento2_layout,container,false);
+    }
 }
